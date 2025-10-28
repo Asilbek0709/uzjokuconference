f626b66d347a62c50fc971e318d05089a6ac66a1
Asilbek0709 <tasilbek348@gmail.com>
2025-10-28 15:57:11 +0500

    Fixes

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -7,8 +7,14 @@ export default function Gaellerypage() {
 
     return(
         <div className='gallery-page'>
-            <h1>Gallery</h1>
-                
+            <div className="gallery-container">
+                <div className="gallery-card"><img src="/image/cphoto1.jpg" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto2.jpg" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto3.jpg" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto4.jpg" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto5.jpg" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto6.png" alt="" /></div>
+            </div>
         </div>
     )
 }
