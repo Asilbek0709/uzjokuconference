70ff86cd09cc9ffd3e5ce2c941f01abacc0f7d3b
Asilbek0709 <tasilbek348@gmail.com>
2025-11-13 12:53:37 +0500

    Articles2

---
+++ src/app/3-variation/page.jsx
@@ -0,0 +1,82 @@
+"use client"
+
+import "../globals.css"
+
+export default function ThirdVariation() {
+
+
+    return(
+        <section>
+            <div className="articleVaritation-list">
+                <div className="articleVariation-container">
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>QPROFESSIONAL KOMPETENTLIK JURNALISTIK FAOLIYATNING ASOSIY MEZONI SIFATIDA</h4>
+                            <h5>Ravshan RAYIMJONOV</h5>
+                            <p>264-273 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/34.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/34.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>PEDAGOGIKALÍQ TERMINOLOGIYANÍŃ MILLIYLIK, MÁDENIYAT HÁM QÁDIRYATLAR MENEN BAYLANÍSÍ</h4>
+                            <h5>Dilfuza SULTANOVA</h5>
+                            <p>274-280 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/35.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/35.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>OʻZBEKISTONDA DAVLAT VA JAMIYAT BOSHQARUVIDA YANGICHA YONDASHUVLAR </h4>
+                            <h5>Shahzoda YOQUBOVA</h5>
+                            <p>281-286 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/36.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/36.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>XURSHID DO‘STMUHAMMAD PUBLITSISTIKASIDA MILLIY O‘ZLIK</h4>
+                            <h5>Ziyoda SAFAROVA</h5>
+                            <p>287-292 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/37.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/37.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>AXBOROT URUSHI VA DEZINFORMATSIYA: “VATANPARVAR” GAZETASI MISOLIDA</h4>
+                            <h5>Ilmira SHUKURLAYEVA</h5>
+                            <p>293-301 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/38.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/38.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>THE ECOLOGICAL AND ENERGY TRANSFORMATION OF UZBEKISTAN WITHIN THE FRAMEWORK OF THE EUROPEAN UNION’S “GREEN FUTURE” INITIATIVE</h4>
+                            <h5>Sеvаrа NОDIRОVА</h5>
+                            <p>302-308 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/39.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/39.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/app/5-variation/page.jsx
@@ -0,0 +1,114 @@
+import "../globals.css"
+
+
+export default function FifthVariation() {
+
+
+    return(
+        <section>
+            <div className="articleVaritation-list">
+                <div className="articleVariation-container">
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>QJAVDAT ABDULLANING IJOD CHASHMASI </h4>
+                            <h5>Gulbahor SAIDG‘ANIYEVA</h5>
+                            <p>635-643 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/82.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/82.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TIL DAVLAT OTINING TIZGINIDIR</h4>
+                            <h5>Bahodir QOBIL</h5>
+                            <p>644-650 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/83.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/83.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>NUTQDA DALIL KELTIRISHNING AHAMIYATI VA TURLARI</h4>
+                            <h5>Gulnoza DJURAKULOVA</h5>
+                            <p>651-658 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/84.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/84.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>INTERNET MULOQOTINING LINGVISTIK XUSUSIYATLARI VA UNING JAMOATCHILIK FIKRIGA TA’SIRI</h4>
+                            <h5>Feruza YAKUBOVA</h5>
+                            <p>659-665 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/85.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/85.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>MULOQOT JARAYONIDA NUTQIY TA’SIRCHANLIKNI OSHIRUVCHI TIL BIRLIKLARI</h4>
+                            <h5>Xurshida ASQAROVA</h5>
+                            <p>666-672 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/86.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/86.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TALABALARDA NOTIQLIK KOMPETENSIYASINI RIVOJLANTIRISHDAGI INNOVATSION YONDASHUVLAR </h4>
+                            <h5>Mahliyo TESHABAYEVA</h5>
+                            <p>673-681 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/87.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/87.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>MEDIAMATNLARDA VIZUAL VOSITALARNING QO‘LLANILISHI (“Jadid” va “Hurriyat” gazetalari misolida) </h4>
+                            <h5>Mahina AXMEDOVA</h5>
+                            <p>682-687 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/88.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/88.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>KUN.UZ VA QALAMPIR.UZ SAYTLARIDAGI REKLAMA MATNLARINING PSIXOLINGVISTIK TAHLILI</h4>
+                            <h5>Dilsora RAXIMOVA</h5>
+                            <p>688-696 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/89.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/89.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>MNUTQ TUZISHNING XUSUSIYATLARI VA NUTQDA RITORIK SHAKILLARDAN FOYDALANISH</h4>
+                            <h5>Nilufar RABBINOVA</h5>
+                            <p>697-702 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/90.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/90.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </section>
+    )
+}
