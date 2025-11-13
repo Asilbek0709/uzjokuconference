38a317510b343ab7ab95f4776806b1760d8b0811
Asilbek0709 <tasilbek348@gmail.com>
2025-11-13 06:39:08 +0500

    Articles1

---
+++ src/app/2-variation/page.jsx
@@ -0,0 +1,236 @@
+"use client"
+
+import "../globals.css"
+
+export default function SecondVariation() {
+
+
+    return(
+        <section>
+            <div className="articleVaritation-list">
+                <div className="articleVariation-container">
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>QODIRIY VA KINOTANQIDCHILIK MASALALARI</h4>
+                            <h5>Latofat TASHMUXAMEDOVA</h5>
+                            <p>114-122 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/14.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/14.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>NAVOIY NASRI: MAKTUBOTLAR KOMPOZITSIYASI</h4>
+                            <h5>Tozagul  DUSANOVA</h5>
+                            <p>123-133 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/15.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/15.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>SHOYIM BO‘TAYEV ROMANLARIDA INSON VA BORLIQ FALSAFASI: IJODIY KONSEPSIYA, RUHIY TAHLIL VA MIFOPOETIK TAFAKKUR (“Qo‘rg‘onlangan oy”, “Shox” romanlari misolida) </h4>
+                            <h5>Elmira DJURAKULOVA</h5>
+                            <p>134-140 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/16.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/16.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TARJIMONNING SHAKLLANISHIDA NOTIQLIK SAN’ATI KO‘NIKMALARI</h4>
+                            <h5>Bobur G‘IYOSOV</h5>
+                            <p>141-145 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/17.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/17.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>O‘ZBEK ADABIYOTIDA BADIIY USLUB HODISASI</h4>
+                            <h5>Yulduz TURDIYEVA</h5>
+                            <p>146-153 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/18.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/18.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>ADABIYOT O‘QITISHDA RAQAMLI TA’LIM TEXNOLOGIYASIDAN FOYDALANISH</h4>
+                            <h5>Mahliyo ELBOYEVA</h5>
+                            <p>154-161 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/19.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/19.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>G‘OYA  TUSHUNCHASINING ADABIY-NAZARIY JIHATDAN O‘RGANILISHI</h4>
+                            <h5>Nafisa KARIMOVA</h5>
+                            <p>162-167 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/20.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/20.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>HALIMA XUDOYBERDIYEVA SHE’RIYATIDA TARIXIYLIK VA SHAXS KONSEPSIYASI</h4>
+                            <h5>Latofat TAJIBAYEVA</h5>
+                            <p>168-174 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/21.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/21.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>ADABIYOT DARSLARIDA MULTIMEDIADAN FOYDALANISHNING NAZARIY VA AMALIY ASOSLARI</h4>
+                            <h5>Sug‘diyona XUDOYNAZAROVA</h5>
+                            <p>177-186 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/22.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/22.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TARJIMA AMALIYOTI VA PUBLITSISTIK MATERIALLARDA INNOVATSION YONDASHUVLAR ASOSIDA FAOLIYAT YURITISH</h4>
+                            <h5>Fazilat EGAMOVA</h5>
+                            <p>187-192 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/23.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/23.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>RAMZIY OBRAZLARNING POETIK XUSUSIYATLARI (Tog‘ay Murodning “Bu dunyoda o‘lib bo‘lmaydi” romani asosida)</h4>
+                            <h5>Iymona MAXMUDOVA</h5>
+                            <p>193-200 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/24.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/24.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>FIQRA JANRINING RIVOJLANISH BOSQICHLARI</h4>
+                            <h5>Rayhona XO‘JAEVA</h5>
+                            <p>201-206 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/25.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/25.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>“ME’ROJNOMAYI TURKIY”DA HAZRATI BIBI FOTIMA ANHONING OTALARIGA MUNOJOTI</h4>
+                            <h5>Saodat LATIPOVA</h5>
+                            <p>207-214 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/26.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/26.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>IBROYIM YUSUPOV ASARLARI TARJIMALARINING BADIIY VA STILISTIK XUSUSIYATLARI</h4>
+                            <h5>Gulsara ATAJANOVA</h5>
+                            <p>215-220 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/27.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/27.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>O‘ZBEK BOLALAR SHE’RIYATIDA BOLA RUHIYATI TASVIRI</h4>
+                            <h5>Madina XOLBOBOYEVA</h5>
+                            <p>221-226 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/28.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/28.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TURK VA O‘ZBEK TILLARIDA MA’NOSI O‘XSHASH BO‘LGAN FRAZEOLOGIZMLARINING UMUMIY VA FARQLI JIHATLARI</h4>
+                            <h5>Saodat SHAMAKSUDOVA & Xurshid DILLIYEV & Sarvinoz BAXODIRJONOVA</h5>
+                            <p>227-232 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/29.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/29.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>OZOD SHARAFIDDINOV TALQINIDA HAMZA HAKIMZODA NIYOZIY SHAXSI VA IJODIY DUNYOSI</h4>
+                            <h5>Shahzoda XALIMOVA</h5>
+                            <p>233-238 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/30.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/30.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>“CHO‘LPONNI ANGLASH” MAQOLASIDA MILLIY TAFAKKUR VA IJODKOR SHAXSIYATINI TALQIN ETISH MASALALARI</h4>
+                            <h5>Shahnoza OCHILDIYEVA</h5>
+                            <p>239-246 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/31.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/31.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>ZAMONAVIY O‘ZBEK TILIDAGI AYRIM KALIMALARNING “DEVONI LUG‘ATIT TURK’’ DA AKS ETISHI</h4>
+                            <h5>Gulnoza ABDURASHIDOVA</h5>
+                            <p>247-254 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/32.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/32.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                    <div className="articleVariation-card">
+                        <div className="article-info">
+                            <h4>TIL VA TAFAKKUR MUSHTARAKLIGI: ARAB VA O‘ZBEK FRAZEOLOGIZMLARINING UMUMIY JIHATLARI</h4>
+                            <h5>Yorqinoy FOZILJONOVA</h5>
+                            <p>255-263 DOI: </p>
+                        </div>
+                        <div className="references">
+                            <a href="/files/33.pdf" download className="reference">Yuklab olish</a>
+                            <a href="/files/33.pdf" target="_blank" rel="noopener norefferer" className="reference">O`qish</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -9,11 +9,27 @@ export default function Gaellerypage() {
         <div className='gallery-page'>
             <div className="gallery-container">
                 <div className="gallery-card"><img src="/image/cphoto1.jpg" alt="" /></div>
-                <div className="gallery-card"><img src="/image/cphoto2.jpg" alt="" /></div>
                 <div className="gallery-card"><img src="/image/cphoto3.jpg" alt="" /></div>
                 <div className="gallery-card"><img src="/image/cphoto4.jpg" alt="" /></div>
                 <div className="gallery-card"><img src="/image/cphoto5.jpg" alt="" /></div>
-                <div className="gallery-card"><img src="/image/cphoto6.png" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto7.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto8.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto9.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto10.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto11.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto12.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto13.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto14.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto15.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto16.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto17.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto18.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto19.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto20.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto22.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto23.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto24.JPG" alt="" /></div>
+                <div className="gallery-card"><img src="/image/cphoto25.JPG" alt="" /></div>
             </div>
         </div>
     )
